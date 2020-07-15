1fcd7379077e29e1ffb1436c7a6d362660febd89
dlgmltjr0925 <dlgmltjr0925@gmail.com>
2020-07-16 02:42:47 +0900

    step 12

--- MemoObjC/Model/Memo.m
+++ MemoObjC/Model/Memo.m
@@ -14,7 +14,7 @@
     self = [super init];
     if (self != nil) {
         _content = content;
-        _insertDate = [NSDate date];
+        _insertDate = [NSDate date]; 
     }
     return self;
 }
