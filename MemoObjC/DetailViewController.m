e0e753bb314e9ec47a90b519a7837e87befbd2d3
Bright <hslee@insterior.co.kr>
2020-08-07 11:06:22 +0900

    complete memo app

--- MemoObjC/DetailViewController.m
+++ MemoObjC/DetailViewController.m
@@ -18,6 +18,7 @@
 
 - (IBAction)deleteMemo:(id)sender;
 
+- (IBAction)share:(id)sender;
 
 @end
 
@@ -76,6 +77,14 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)share:(id)sender {
+    NSString *memo = self.memo.content;
+    
+    UIActivityViewController *vc = [[UIActivityViewController alloc] initWithActivityItems:@[memo] applicationActivities:nil];
+    
+    [self presentViewController:vc animated:YES completion:nil];
+}
 
 - (IBAction)deleteMemo:(id)sender {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"삭제 확인" message:@"메모를 삭제할까요?" preferredStyle:UIAlertControllerStyleAlert];
